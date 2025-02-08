c43505a6b0af353447e02380ea16da2103eb2683
Bram Adams <3282661+bramses@users.noreply.github.com>
2025-02-08 15:30:28 -0500

    Add node-fetch dependency and enhance CopyButton: open share URL in a new tab on click for improved user experience

--- src/components/CopyButton.tsx
+++ src/components/CopyButton.tsx
@@ -9,12 +9,14 @@ type CopyButtonProps = {
 
 const CopyButton: React.FC<CopyButtonProps> = ({ id }) => {
   const [showStatus, setShowStatus] = useState(false);
+
   return (
     <><button
       type="button"
       className='button type="button" class="focus:outline-none text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2'
       onClick={() => {
         navigator.clipboard.writeText(id);
+        window.open(`${process.env.NEXT_PUBLIC_COMPANION_URL}/dashboard/?share=${id}`, "_blank");
         setShowStatus(true);
       }}
     >
